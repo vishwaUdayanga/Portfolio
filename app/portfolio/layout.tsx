4563f160c895247ba81382abe79559e4d75a9e19
Vishwa Udayanga <udhayangavishwa@gmail.com>
2024-11-24 23:16:58 +0530

    change

--- app/portfolio/layout.tsx
+++ app/portfolio/layout.tsx
@@ -14,23 +14,23 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     }
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-        <div className={
-            clsx(
-                'w-full transition-width duration-300 ease-in-out flex-none',
-                { 'md:w-20': !toggle,
-                'md:w-64': toggle
-                }
-            )
-            } 
-        >
-            <SideNav isToggle={toggle} />
-        </div>
-        <div className="flex-grow md:overflow-y-auto">
-            <div className="w-full justify-between items-center p-4 hidden md:flex">
-            <Image src="/portfolio/menu.png" alt="Menu" width={15} height={15} onClick={handleToggle} className="cursor-pointer"/>
+            <div className={
+                clsx(
+                    'w-full transition-width duration-300 ease-in-out flex-none md:h-screen md:overflow-auto',
+                    { 'md:w-20': !toggle,
+                    'md:w-64': toggle
+                    }
+                )
+                } 
+            >
+                <SideNav isToggle={toggle} />
             </div>
-            {children}
-        </div>
+            <div className="flex-grow md:overflow-y-auto">
+                <div className="w-full justify-between items-center p-4 hidden md:flex">
+                <Image src="/portfolio/menu.png" alt="Menu" width={15} height={15} onClick={handleToggle} className="cursor-pointer"/>
+                </div>
+                {children}
+            </div>
         </div>
     );
 };
